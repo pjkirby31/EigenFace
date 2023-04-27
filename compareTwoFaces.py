ce5f092651ea50a7f1686586f1435150159d7ede
Patrick Kirby <61844179+pjkirby31@users.noreply.github.com>
2023-04-27 12:05:39 -0500

    Added comments

--- compareTwoFaces.py
+++ compareTwoFaces.py
@@ -1,28 +1,49 @@
 from PIL import Image
 import numpy as np
 
-grey_img_1 = Image.open("data1Cropped/img008.jpg").convert("L")
+## Determines whether or not to show images
+show_images = False
+num_comps = 50
+
+## Opens images and converts them to greyscale
+grey_img_1 = Image.open("data1Cropped/img003.jpg").convert("L")
 pixel_array_1 = np.asarray(grey_img_1).astype(np.uint8)
 
-grey_img_2 = Image.open("data1Cropped/img008.jpg").convert("L")
+grey_img_2 = Image.open("data1Cropped/img002.jpg").convert("L")
 pixel_array_2 = np.asarray(grey_img_2).astype(np.uint8)
 
+## Performs SVD on both images separately
+## Then reconstructs the images with a set number of components
 u1, s1, v1 = np.linalg.svd(pixel_array_1,full_matrices=False)
-num_comps = 50
 remade1 = np.matrix(u1[:,:num_comps]) * np.diag(s1[:num_comps]) * np.matrix(v1[:num_comps, : ])
-# new_image1 = Image.fromarray(remade1)
-# new_image1.show()
+
 u2, s2, v2 = np.linalg.svd(pixel_array_2,full_matrices=False)
 remade2 = np.matrix(u2[:,:num_comps]) * np.diag(s2[:num_comps]) * np.matrix(v2[:num_comps, : ])
-# new_image2 = Image.fromarray(remade2)
-# new_image2.show()
+
+## Shows the images if you want to see who they are
+if show_images:
+    new_image1 = Image.fromarray(remade1)
+    new_image2 = Image.fromarray(remade2)
+    new_image1.show()
+    new_image2.show()
+    
+## Reshapes the images so they are column vectors
 flattened_array_1 = np.reshape(np.asarray(remade1),[901*1201])
 flattened_array_2 = np.reshape(np.asarray(remade2),[901*1201])
-print(flattened_array_1.shape)
-print(flattened_array_2.shape)
 
+## Performs cosine similarity by finding angle between vectors
 dot_product = np.dot(flattened_array_1, flattened_array_2)
-print(dot_product)
 cosine_similarity = dot_product / (np.linalg.norm(flattened_array_1)*np.linalg.norm(flattened_array_2))
 
-print(cosine_similarity)
+print("Cosine similarity after " + str(num_comps) + " component SVD: " + str(round(cosine_similarity,3)*100) + "%")
+
+
+flattened_array_1 = np.reshape(np.asarray(pixel_array_1),[901*1201])
+flattened_array_2 = np.reshape(np.asarray(pixel_array_2),[901*1201])
+
+## Performs cosine similarity by finding angle between vectors
+dot_product = np.dot(flattened_array_1, flattened_array_2)
+cosine_similarity = dot_product / (np.linalg.norm(flattened_array_1)*np.linalg.norm(flattened_array_2))
+
+print("Cosine similarity of raw images:          "  + str(round(cosine_similarity,3)*100) + "%")
+print("Actual value: " + str(cosine_similarity * 100) + "%")
--- processImages.py
+++ processImages.py
@@ -3,21 +3,30 @@ from PIL import Image
 import numpy as np
 import random
 
+## Lists all of the images in the data folder
 file_path = "data1Cropped/"
 image_list = os.listdir(file_path)
+
+## Initializes an empty matrix for the images to be put in
 img_matrix = np.empty([901*1201,len(image_list)])
 
 index = 0
 print("Compressing images into one matrix")
 
 for image in image_list:
+    ## Opens an image and converts it to greyscale
     grey_img = Image.open(file_path + image).convert("L")
+    ## Converts the datatype of the image and saves it as a numpy array
     pixel_array = np.asarray(grey_img).astype(np.uint8)
     
+    ## Finds the height and width of the image (1201,901)
     width = len(pixel_array[0])
     height = len(pixel_array)
     
-    flattened_array = np.reshape(pixel_array.T,[901*1201]).astype(np.uint8)
+    ## Reshapes the image array to be a column vector that is 901*1201 long
+    flattened_array = np.reshape(pixel_array.T,[901*1201])
+    
+    ## Inserts the column vector as a column of the large image matrix
     img_matrix[:,index] = flattened_array
     index+=1
     
@@ -25,6 +34,8 @@ print("Matrix shape: " + str(img_matrix.shape))
 print("Number of images in dataset: " + str(len(img_matrix[0,:])))
 print("Performing SVD")
 
+## Performs SVD on the large image matrix
+## Setting full_matrices=False is kind of like "econ" in Matlab
 u, s, v = np.linalg.svd(img_matrix,full_matrices=False)
 
 print("S shape: " + str(s.shape))
@@ -33,12 +44,24 @@ print("V shape: " + str(v.shape))
 print("Reconstructing faces")
 
 for num_comps in range(1,53):
-    
+    ## Remakes the large image matrix by multiplying U*S*V'
+    ## np.matrix converts U,S,V to a matrix  that can be multiplied easier
     remade = np.matrix(u[:,:num_comps]) * np.diag(s[:num_comps]) * np.matrix(v[:num_comps, : ])
+    
+    ## Selects a random person to show
     personToShow = random.randint(0,53)
+    
+    ## Extracts the random column and reshapes it to original size
     reshaped = remade[:,personToShow].reshape(901,1201)
+    
+    ## Converts the numpy matrix to an Image and rotates it
     new_image = Image.fromarray(reshaped).rotate(270, Image.NEAREST, expand = 1)
+    
+    ## Converts the image to be RGB
+    ## Image is still greyscale, but this just changed how it is stored
     if new_image.mode != 'RGB':
         new_image = new_image.convert('RGB')
+        
+    ## Saves image to a new folder with the component name
     new_image.save("diffPeople/" + str(num_comps) + "_comps.jpg")
     print("Saved image: " + "diffPeople/" + str(num_comps) + "_comps.jpg")
